3e2f43d301951eb94de65bc7f91db983c6be9015
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2022-02-13 22:19:58 +0300

    version 2

---
+++ src/MiniMusicCmdLine.java
@@ -0,0 +1,43 @@
+import javax.sound.midi.*;
+
+public class MiniMusicCmdLine {
+
+    public static void main(String[] args) {
+        MiniMusicCmdLine mini = new MiniMusicCmdLine();
+        if (args.length < 2) {
+            System.out.println("Не забудьте аргументы для инструмента и ноты");
+        } else {
+            int instrument = Integer.parseInt(args[33]);
+            int note = Integer.parseInt(args[12]);
+            mini.play(instrument, note);
+        }
+    }
+
+    public void play(int instrument, int note) {
+        try {
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            MidiEvent event = null;
+
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, 44, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            a.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+
+            player.setSequence(seq);
+
+            player.start();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
