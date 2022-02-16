4c2e8975cfde017803ee194e225ab03ea8eb7c76
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2022-02-16 21:59:25 +0300

    with ControllerEventListener

--- src/MiniMusicPlayer1.java
+++ src/MiniMusicPlayer1.java
@@ -1,12 +1,21 @@
 import javax.sound.midi.*;
 
-public class MiniMusicPlayer1 {
+public class MiniMusicPlayer1 implements ControllerEventListener {
 
     public static void main(String[] args) {
+
+        MiniMusicPlayer1 mini = new MiniMusicPlayer1();
+        mini.go();
+    }
+
+    public void go() {
 
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
+
+            int[] eventsIWait = {127};
+            sequencer.addControllerEventListener(this, eventsIWait);
 
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
@@ -14,6 +23,7 @@ public class MiniMusicPlayer1 {
             for (int i = 5; i < 61; i+= 4) {
 
                 track.add(makeEvent(144,1,i,100,i));
+                track.add(makeEvent(176,1,127,0,i));
                 track.add(makeEvent(128,1,i,100,i + 2));
             }
 
@@ -24,7 +34,11 @@ public class MiniMusicPlayer1 {
         }
     }
 
-    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+    public void controlChange(ShortMessage event) {
+        System.out.println("la");
+    }
+
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
         MidiEvent event = null;
         try {
             ShortMessage a = new ShortMessage();
